668585f4d0a1bbcbd6ef2935885fbdfa7804379b
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-11-21 07:35:18 -0800

    DBStorage

--- models/state.py
+++ models/state.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
+from models.base_model import Base
 
-
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ State class for a MySQL database"""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
